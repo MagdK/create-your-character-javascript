38dab1ef19501d9e601a2dfc913c8370ad1cc453
Magda Kovács <kovacsmagda@gmail.com>
2022-03-29 10:27:01 +0200

    Upload missing images

--- README.md
+++ README.md
@@ -1,9 +1,43 @@
-# Pretty Simple Web server written in Node.js
+# Create Your Character
 
-You can use this repository as a template for your repository or copy the files into your repository manually
+## Story
 
-1. Open a new terminal (eg. in VS Code when your repository's directory opened)
-2. Run with the following command: `node backend/server.js` (Make sure that you are in the right directory.)
-3. If you don't need it stop it with `Control + C` keycommand.
+You are working in an online game developer company that is about to release a new RPG game.
+In the developer team, you've got the task to implement the character creation part.
 
-You can write your frontend code (`HTML, CSS, JS`) into the `frontend` directory.
+The task is to provide a form that the users can fill up to create their character.
+Whenever they fill a character property, next to the form, the character sheet will fill up too.
+
+## What are you going to learn?
+
+- DOM manipulation
+- `input` event
+- Dynamic page changing
+
+## Tasks
+
+1. Create a form that the user can fill up. A character needs to have a name, a choosable gender, race and class.
+    - When we open up the HTML file, a form shows up
+    - The form contains an input field for the character name
+    - We can choose from dropdown menus for the gender, for the race and for the class
+
+2. Whenever an input changes, the character sheet next to the form will fill up with the change. Use icons or images for the character sheet when you want to display the dropdown menus value.
+    - When we change the name or a dropdown menu value,  the value shows up next to the form in a character sheet
+    - For the gender, the race and class, the value is represented with an icon or an image
+
+## General requirements
+
+None
+
+## Hints
+
+- Use the `input` event whenever you use an `input` field or a `select` tag for the purpose of catching the tags value change effect.
+  Unlike the `change` event, the `input` is triggered whenever the value changed.
+
+## Background materials
+
+- <i class="far fa-exclamation"></i> [Input event](https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/input_event)
+- <i class="far fa-video"></i> [Introduction to DOM manipulation](https://www.youtube.com/watch?v=wiozYyXQEVk&ab_channel=DevEd)
+- <i class="far fa-exclamation"></i> [DOM manipulation](https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Introduction)
+- <i class="far fa-exclamation"></i> [Javascript if/else](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else)
+- [Sample for races and classes](https://5e.tools/)
---
+++ frontend/script.js
@@ -0,0 +1,76 @@
+// COMPONENTS
+
+let characterEditBlocks = function() {
+    return `
+    <h1 id="page-title">Create Your Character</h1>
+    <section id="character-creation">
+        <article class="edit-block">
+            <form>
+                <label for="name">name</label>
+                <input type="text" id="name" name="name">
+                <label for="gender">Gender</label>
+                <select id="gender" name="gender">
+                    <option value="G-Default">Default</option>
+                    <option value="G-Girl">Girl</option>
+                    <option value="G-Boy">Boy</option>
+                    <option value="G-Prefer-not-to-say">Prefer not to say</option>
+                </select>
+                <label for="race">Race</label>
+                <select id="race" name="race">
+                    <option value="R-Default">Default</option>
+                    <option value="R-Zombie">Zombie</option>
+                    <option value="R-Gold">Gold</option>
+                </select>
+                <label for="class">Class</label>
+                <select id="class" name="class">
+                    <option value="C-Default">Default</option>
+                    <option value="C-Dealer">Dealer</option>
+                    <option value="C-Floater">Floater</option>
+                    <option value="C-Royal">Royal</option>
+                </select>
+            </form>
+        </article>
+        <article class="preview-block">
+            <img src="assets/R-Default-G-Default-C-Default.svg" alt="My Happy SVG" id="monkey-img" />
+            <h2 id="name-value"></h2>
+        </article>
+    </section>
+    `;
+};
+
+
+function loadEvent() {
+    let rootElement = document.getElementById("root");
+    // console.log(rootElement);
+
+    rootElement.insertAdjacentHTML("beforeend", characterEditBlocks());
+
+            // NAME INPUT
+    const input = document.querySelector('input');
+    const log = document.getElementById('name-value');
+
+    input.addEventListener('input', updateValue);
+
+    function updateValue(e) {
+      log.textContent = e.target.value;
+    }
+
+            // SELECT
+    const genderSelect = document.getElementById('gender');
+    const raceSelect = document.getElementById('race');
+    const classSelect = document.getElementById('class');
+    const image = document.getElementById('monkey-img');
+ 
+    genderSelect.addEventListener('change', updateImage);
+    raceSelect.addEventListener('change', updateImage);
+    classSelect.addEventListener('change', updateImage);
+ 
+    function updateImage(e) {
+       const values = [raceSelect.value, genderSelect.value, classSelect.value];
+       const imagePath = "assets/" + values.join("-") + ".svg";
+
+       image.src = imagePath
+    }
+}
+window.addEventListener("load", loadEvent);
+
