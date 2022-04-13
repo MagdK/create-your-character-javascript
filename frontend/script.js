46ae38a51f16284ead254a454cc5170904969b74
Magda Kovács <kovacsmagda@gmail.com>
2022-04-13 19:38:29 +0200

    if else solution

--- frontend/script.js
+++ frontend/script.js
@@ -10,28 +10,15 @@ let characterEditBlocks = function() {
                 <input type="text" id="name" name="name">
                 <label for="gender">Gender</label>
                 <select id="gender" name="gender">
-                    <option value="G-Default">Default</option>
-                    <option value="G-Girl">Girl</option>
-                    <option value="G-Boy">Boy</option>
-                    <option value="G-Prefer-not-to-say">Prefer not to say</option>
-                </select>
-                <label for="race">Race</label>
-                <select id="race" name="race">
-                    <option value="R-Default">Default</option>
-                    <option value="R-Zombie">Zombie</option>
-                    <option value="R-Gold">Gold</option>
-                </select>
-                <label for="class">Class</label>
-                <select id="class" name="class">
-                    <option value="C-Default">Default</option>
-                    <option value="C-Dealer">Dealer</option>
-                    <option value="C-Floater">Floater</option>
-                    <option value="C-Royal">Royal</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
                 </select>
             </form>
         </article>
         <article class="preview-block">
-            <img src="assets/R-Default-G-Default-C-Default.svg" alt="My Happy SVG" id="monkey-img" />
+            <img src="assets/monkey-1.png" alt="My Happy SVG" id="monkey-img" />
             <h2 id="name-value"></h2>
         </article>
     </section>
@@ -55,21 +42,25 @@ function loadEvent() {
       log.textContent = e.target.value;
     }
 
-            // SELECT
+    // SELECT
     const genderSelect = document.getElementById('gender');
-    const raceSelect = document.getElementById('race');
-    const classSelect = document.getElementById('class');
+    
     const image = document.getElementById('monkey-img');
  
     genderSelect.addEventListener('change', updateImage);
-    raceSelect.addEventListener('change', updateImage);
-    classSelect.addEventListener('change', updateImage);
+    
  
-    function updateImage(e) {
-       const values = [raceSelect.value, genderSelect.value, classSelect.value];
-       const imagePath = "assets/" + values.join("-") + ".svg";
-
-       image.src = imagePath
+    function updateImage() {
+       // if else goes here
+       if (genderSelect.value === '1') {
+        image.scr = "assets/monkey-1.png"
+       } else if( genderSelect.value === '2') {
+        image.src = "assets/monkey-2.png"
+       } else if( genderSelect.value === '3') {
+        image.src = "assets/monkey-3.png"
+       } else if(genderSelect.value === '4'){
+       image.src = "assets/monkey-4.png"
+       }
     }
 }
 window.addEventListener("load", loadEvent);
