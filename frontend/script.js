7ce1a3db410cb002bcfa298e2acde6ced4bac67e
Magda Kovács <kovacsmagda@gmail.com>
2022-04-14 17:13:27 +0200

    Clean out loader animation

--- frontend/script.js
+++ frontend/script.js
@@ -38,7 +38,11 @@ let characterEditBlocks = function() {
     `;
 };
 
-
+ // LOADING ANIMATION
+function showPage() {
+     document.getElementById("loader").style.display = "none";
+     document.getElementById("root").style.display = "block";
+};
 
 function loadEvent() {
     let rootElement = document.getElementById("root");
@@ -127,17 +131,7 @@ function loadEvent() {
         }
     }
 
-     // LOADING ANIMATION
-     let myVar;
-
-     function myFunction() {
-         myVar = setTimeout(showPage, 3000);
-     }
- 
-     function showPage() {
-         document.getElementById("loader").style.display = "none";
-         document.getElementById("root").style.display = "block";
-     }
+    setTimeout(showPage, 3000);
 };
 window.addEventListener("load", loadEvent);
 
